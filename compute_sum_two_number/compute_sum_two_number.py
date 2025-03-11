9ff4ef286a437fde1e5cc96b6ba8a2252c16b2b2
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 19:57:22 +0800

    prog 3 pseudo and code

---
+++ compute_sum_two_number/compute_sum_two_number.py
@@ -0,0 +1,7 @@
+# ask user to input two numbers
+input_number_one = float(input("Input First Number:"))
+input_number_two = float(input("Input Second Number:"))
+# calculate the sum of inputted numbers
+sum = input_number_one+input_number_two
+# print sum of inputted numbers
+print(sum)   
