951ca71d1cc33d3586c8b68da30d8bd5f0420c6d
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 20:12:56 +0800

    prog 2 pseudo and code

--- check_and_print_number_if_equal/check_and_print_number_if_equal.py
+++ check_and_print_number_if_equal/check_and_print_number_if_equal.py
@@ -1,10 +1,11 @@
 # ask user to input 
 input_number_one = int(input("Input First Number:"))
 input_number_two = int(input("Input Second Number:"))
+
 # check if inputted two numbers are the same
-# if same, print "Equal"
 if input_number_one == input_number_two:
-    print("Equal")
-#if not same, print "Not Equal"    
+    # if same, print "Equal"
+    print("Equal")    
 else:
+    #if not same, print "Not Equal"
     print("Not Equal")
